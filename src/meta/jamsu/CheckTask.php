38222e309192badf74d75921234884a353b38173
AndEd <zx3211226@gmail.com>
2017-04-17 20:53:44 +0900

    Update CheckTask.php

--- src/meta/jamsu/CheckTask.php
+++ src/meta/jamsu/CheckTask.php
@@ -1,1 +1,15 @@
 <?php
+namespace meta\jamsu;
+
+use pocketmine\scheduler\Task;
+
+class CheckTask extends Task {
+	private $owner;
+	public function __construct(JamSuKick $owner) {
+		$this->owner = $owner;
+	}
+	public function onRun($currentTick) {
+		$this->owner->checkTick ();
+	}
+}
+?>
